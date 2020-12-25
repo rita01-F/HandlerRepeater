4fb2b9b102dec87e6ec3ceec1b7bf9e05397b2fc
rita01-F <63978286+rita01-F@users.noreply.github.com>
2020-12-25 19:33:08 +1000

    change
    s

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 addEventListener('mouseup', function (event) {
-    a.push(event);
+    a.push(event.target);
 }, true);
 
 
 $(document).keyup(function (event) {
     if (event.key === "Escape" || event.keyCode === 27) {
         for (let i = 0; i < a.length; i ++) {
-            a[i].target.click();
+            a[i].click();
         }
     }
 });
